b1d31db3b77658e550a4e9733642990ad6474d4d
keshavpar <keshavparihar54@gmail.com>
2023-09-15 09:25:10 +0530

    added JWT for Signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,52 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('./../models/user');
 
 const asyncErrorHandler = require('./../utils/asyncErrorHandler');
 const CustomError = require('./../utils/customError');
 
-exports.signup = asyncErrorHandler( async (req, res, next) => {
-    const user = await User.create(req.body);
+const signToken = id => {
+    console.log(process.env.SECRET_STR);
+    console.log(process.env.TOKEN_EXPIRES_IN);
+    return jwt.sign({id}, process.env.SECRET_STR, {
+        expiresIn: process.env.TOKEN_EXPIRES_IN
+    })
+};
 
-    res.status(201).json({
+const createSendToken = (user, statusCode, res) => {
+    const token = signToken(user._id);
+
+    res.status(statusCode).json({
         status: 'success',
+        token,
         data: {
             user
         }
     });
+}
+
+exports.signup = asyncErrorHandler( async (req, res, next) => {
+    const user = await User.create(req.body);
+
+    // Sending  jwt token to directly redirect to login
+   createSendToken(user, 201, res);
 })
 
 exports.login = asyncErrorHandler( async (req, res, next) => {
-    // 1. Check if user exists with gievn mail 
+    // 1. Check if user email or password present in request body or not
+    const email = req.body.email;
+    const password = req.body.password;
 
-    // 2. match the passwords
+    if(!email || !password){
+        next(new CustomError("Use valid credentials to login ! "));
+    }
 
-    // 3. send jwt token
+    // 2. Check if user exists with gievn mail
 
-    // 4. Logged In
+    // 3. match the passwords
+
+    // 4. send jwt token
+
+    // 5. Logged In
 
 })
