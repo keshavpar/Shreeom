1643fb0ebd72474e6913ac01bfe220dc5b9d81b9
Shubham Goswami <shubham24976@gmail.com>
2023-09-16 09:28:00 +0530

    Added Doctor Model

---
+++ models/doctor.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// name; registration No.; BOARD; Degree; Address; PhoneNumber
+const doctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter the Doctor name']
+    },
+    registration_No: {
+        type: String,
+        default: ''
+    },
+    board: {
+        type: String,
+        default: ''
+    },
+    degree: {
+        type: String,
+        default: '',
+    },
+    Address: {
+        type: String,
+        default: ''
+    },
+    phone: {
+        type: String,
+        default: ''
+    }
+});
+
+const Doctor = mongoose.model('Doctor', doctorSchema);
+
+module.exports = Doctor;
